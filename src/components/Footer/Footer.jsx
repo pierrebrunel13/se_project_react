f3987f71eefc1a5e334e81ed8d502a7b02273b2d
Pierrebrunel <pierrebruneljoseph13@gmail.com>
2025-08-14 14:46:43 -0400

    fixed the modalwithform.css project10

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer__container">
-        <span className="footer__text">Developed by Pierrebrunel Joseph</span>
-        <span className="footer__year"> {new Date().getFullYear()}</span>
+        <p className="footer__text">Developed by Pierrebrunel Joseph</p>
+        <p className="footer__year"> {new Date().getFullYear()}</p>
       </div>
     </footer>
   );
